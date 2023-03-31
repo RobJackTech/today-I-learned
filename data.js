396c3c1988abd412c742b8cf53e398794812dc10
RobJackTech <robjacktech@gmail.com>
2023-03-30 22:24:50 -0400

    Styling Buttons

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 const CATEGORIES = [
   { name: "technology", color: "#3b82f6" },
-  { name: "science", color: "#16a34a" },
+  { name: "science", color: #16a34a"" },
   { name: "finance", color: "#ef4444" },
   { name: "society", color: "#eab308" },
   { name: "entertainment", color: "#db2777" },
